034199ea63ab2e0a395a3839767e07f8f8c6eec3
Royal51651 <georgeeggers682@gmail.com>
2024-12-09 16:46:30 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "This Does Some Stuff Lo-Key"
-sleep 5
+echo "Ubuntu System Hardening Script For Cyber Patriot Because Yay :)"
+sleep 2
 # installs 
 sudo apt install clamav
 sudo apt install ufw
 sudo apt install locate
+sudo apt-get install auditd
+sudo apt-get install bum in
 # firewall config
 sudo ufw enable
 sudo ufw default deny incoming
@@ -14,7 +16,17 @@ clamscan
 # permissions
 sudo chmod 644 /etc/passwd
 sudo chmod 600 /etc/shadow
+# pam files and lightdm.conf (pam files cause a lot of errors so we not doing those)
+# sudo chmod g-w common-auth
+# sudo chmod o+w common-password
+# sudo cp -f "common-password" "/etc/pam.d"
+# sudo cp -f "common-auth" "/etc/pam.d"
+echo "allow-guest=false" | sudo cat >> /etc/lightdm/lightdm.conf
+# audits
+sudo auditctl -e 1
 # post instructions
+
 echo "Change Firefox settings to be better"
 echo "Make sure to update"
 echo "locate *.mp3 and delete everything that shows up"
+echo "use bum to start any services that may need to be started"
